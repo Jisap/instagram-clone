04af5bfc8102c898d21544020970cb272b5f91ad
Jorge Matos <jisapjisap@gmail.com>
2023-11-06 18:11:39 +0100

    appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -16,11 +16,14 @@ import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
 import { createUserAccount } from "@/lib/appwrite/api";
+import { useToast } from "@/components/ui/use-toast"
+
 
 
 
 const SignupForm = () => {
 
+  const { toast } = useToast();
   const isLoading = false;
 
   // 1. Define your form.
@@ -39,8 +42,12 @@ const SignupForm = () => {
     const newUser = await createUserAccount(values);
     
     if(!newUser){
-      return
+      return toast({
+        title: "Sign up failed. Please try again.",
+      })
     }
+
+    //const session = await signInAccount()
   }
 
   return (
