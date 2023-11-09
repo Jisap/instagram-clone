2c63c9f176178c4ce4dae1f00007083d1c37c0eb
Jorge Matos <jisapjisap@gmail.com>
2023-11-09 17:50:54 +0100

    rootLayout

---
+++ src/components/shared/Topbar.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { Button } from "../ui/button";
+
+
+const Topbar = () => {
+  return (
+    <div>Topbar</div>
+  )
+}
+
+export default Topbar
